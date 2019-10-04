a5694165a0b36da720ad43536c2c8e7c73ee0b43
sharry2356 <44281100+sharry2356@users.noreply.github.com>
2019-10-04 12:21:40 -0400

    Add files via upload

---
+++ Jaganathan/JagPyH5Batch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /projects/harrys/JagsFIX/JagFINAL/.
+#~/anaconda3/bin/python JaganathanRecreateProcessing_H5Batch.py
+~/anaconda2/bin/python JaganathanRecreateTraining_H5Batch_PY2.py 
+#~/anaconda3/bin/python JaganathanRecreateTesting_H5Batch.py
